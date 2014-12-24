48bdca13bf8c6ae662503c2e6d669e7018770e73
aidan coyne <coynea90@gmail.com>
2014-12-24 09:12:35 -0500

    add documentation

--- README.md
+++ README.md
@@ -1,4 +1,87 @@
 wai-hmac-auth [![Build Status](https://travis-ci.org/raptros/wai-hmac-auth.svg?branch=master)](https://travis-ci.org/raptros/wai-hmac-auth)
 =============
+this library provides functions for authenticating HMAC-signed requests in WAI
+apps. in particular, it provides a function for extracting an api key from a
+request according to configuration, and a function for verifying that a request
+is correctly signed by a secret key, according to the configuration. it is
+designed to be compatible with the Java library
+[jersey-hmac-auth](https://github.com/bazaarvoice/jersey-hmac-auth), though
+possibly more flexible.
 
-a toolkit for doing hmac auth in your WAI apps.
+how it signs
+------------
+the authenticate function (as well as the included signRequest function) in
+effect extracts several elements from the request and concatentates them, and
+hashes/signs the resulting value. i.e. it constructs the following value from
+the request, and signs that value.
+
+```
+valueToSign = method + '\n' + 
+              timestamp + '\n' + 
+              (apiKey + '\n')? + 
+              path + ('?' + query) + '\n' +
+              body
+```
+
+* the method is added directly from the request record.
+* the header name used to get the timestamp value can be configured. note that
+  for now, no validation is done on this value; it is only required to be
+  present in the request.
+* the api key is only included explicitly if the configuration specifies that
+  the api key is in a header. otherwise (if the api key is in a query
+  parameter), it will be included anyway.
+* the value of the path is whatever the application receives in the request
+  record. this means that e.g. if the application is run in the context of a
+  CGI executable, this may not be the same path that the client made the
+  request to. the client will have to account for this when generating the
+  signature.
+* the query string is rendered from the queryString using the http-types
+  function renderQuery. the wai-hmac-auth library does not modify these values,
+  but the values received by the app may depend on the server and the
+  middleware. the client will have to account for this properly.
+* the entire request body is read, which is why authenticate produces a Request
+  value when successful - this Request value has a requestBody value that will
+  produce the same chunk sequence as the input request value.
+
+the authenticate function also extracts the configured signature header and
+attempts to decode it as a base64 url-encoded hash digest value as produced by
+the configured hash function. this represents two possible authentication
+failures.
+
+finally, the authenticate function compares the passed signature to the
+signature it generated and fails if they do not match.
+
+how to use
+----------
+there are basically four steps for using this in your app.
+
+* set up the configuration for how requests to your app will be signed
+* when a request comes in, use getApiKey with your spec to find out who the
+  requester is claiming to be
+* load up the secret key associated with that identity (api key)
+* use that secret key to run the authenticate function on the request
+* send back appropriate responses for errors, otherwise continue with the
+  transformed and authenticated request
+
+for example:
+
+```haskell
+-- this would throw an error, i guess
+demandApiKey :: IO a
+
+-- 
+informClientOfAuthenticationFailure :: AuthFailure -> IO a
+
+-- imagine a function like this
+getSecretOrFail :: ApiKey -> IO SecretKey
+
+handleReq req = do
+    apiKey <- getApiKey defaultApiKeySpec req >>= maybe demandApiKey return
+    -- either get a caller reference or fail the request
+    secretKey <- getSecretOrFail apiKey
+    authRes <- authenticate defaultRequestConfig req secretKey
+    req' <- either informClientOfAuthenticationFailure return authRes
+    -- continue processing using the new request value.
+```
+
+
--- src/Network/Wai/Auth/HMAC.hs
+++ src/Network/Wai/Auth/HMAC.hs
@@ -30,15 +30,24 @@ import Network.HTTP.Types.URI (renderQuery)
 
 -- * types
 
+-- | a newtype wrapper for api keys
 newtype ApiKey = ApiKey BS.ByteString deriving (Eq, Show)
 
+-- | newtype wrapper for secret keys
 newtype SecretKey = SecretKey BS.ByteString deriving (Eq, Show)
 
+-- | specification for how the api key should be found in the request
 data ApiKeySpec = 
+    -- | look for a query parameter with the specified name
     QueryParamKey BS.ByteString |
+    -- | look for the header with this name
     HeaderKey HeaderName
     deriving (Eq, Show)
 
+-- | request configuration specifies how to perform hmac signing and
+-- authentication on the request - i.e. where the api key will be found,
+-- where the timestamp is stored, how the signature is added to the
+-- request, and the hash algorithm to use.
 data RequestConfig alg = RequestConfig {
     keySpec :: ApiKeySpec,
     timestampHeader :: HeaderName,
@@ -46,42 +55,75 @@ data RequestConfig alg = RequestConfig {
     hashAlgorithm :: alg
 } deriving (Eq, Show)
 
+-- | all of the way that signing or authentication can fail.
 data AuthFailure =
+    -- | the request does not have an api key value that fits the spec
     MissingApiKey ApiKeySpec |
+    -- | the request does not have a timestamp header
     MissingTimestampHeader HeaderName |
+    -- | the request does not have a signature header ('authenticate' only)
     MissingSignatureHeader HeaderName |
+    -- | the signature was not url-safe base 64 encoded properly
+    -- (authenticate only)
     SignatureBase64DecodeFailed String |
+    -- | the signature was not a properly encoded hash digest (e.g. for the
+    -- hash algorithm being used) (authenticate only)
     SignatureToDigestFailed |
+    -- | the signature generated from the request did not match the
+    -- signature contained within the reqest (authenticate only)
     HashMismatch
     deriving (Eq, Show)
 
 -- ** constrain aliases
 
+-- | a constraint alias for functions that need to access request
+-- configuration
 type HasReqConf alg m = (HashAlgorithm alg, Mtl.MonadReader (RequestConfig alg) m, Functor m)
 
+-- | a constrain alias for functions that can fail
 type AuthErrorsM m = (Mtl.MonadError AuthFailure m)
 
+-- | a constraint alias for functions that save chunks of the request body
 type WriteChunks m = (Mtl.MonadWriter (S.Seq BS.ByteString) m)
 
+-- | a constrain alias for functions that perform incremental updates to
+-- the hash value
 type HmacState alg m = (Functor m, Applicative m, HashAlgorithm alg, Mtl.MonadState (HMACContext alg) m)
 
 -- ** default setup
 
+-- | default request configuration
+--
+-- @
+-- defaultRequestConfig = 'RequestConfig' 'defaultApiKeySpec' "x-auth-timestamp" "x-auth-signature" 'SHA256'
+-- @
 defaultRequestConfig :: RequestConfig SHA256
-defaultRequestConfig = RequestConfig defaultApiKeySpec "x-auth-timestamp" "authorization" SHA256
+defaultRequestConfig = RequestConfig defaultApiKeySpec "x-auth-timestamp" "x-auth-signature" SHA256
 
+
+-- | default spec for getting the api key
+--
+-- @
+-- defaultApiKeySpec = 'QueryParamKey' "apiKey"
+-- @
 defaultApiKeySpec :: ApiKeySpec
-defaultApiKeySpec = HeaderKey "x-auth-key"
+defaultApiKeySpec = QueryParamKey "apiKey"
 
 -- * the tools 
 
+-- | use this to get the api key from the request according to spec
 getApiKey :: ApiKeySpec -> Request -> Maybe ApiKey
 getApiKey (QueryParamKey k) = fmap ApiKey . join . lookup k . queryString
 getApiKey (HeaderKey k) = fmap ApiKey . lookup k . requestHeaders
 
+-- | authenticate the request according to the configuration and secret
+-- key. if it succeeds, produces a request with a requestBody that will
+-- produce the same chunk sequence as the original. if it fails, it will
+-- explain why.
 authenticate :: HashAlgorithm alg => RequestConfig alg -> Request -> SecretKey -> IO (Either AuthFailure Request)
 authenticate conf req k = runReaderT (runExceptT (checkRequestHmac req k)) conf
 
+-- | the operation performed by 'authenticate'
 checkRequestHmac :: (MonadIO m, HasReqConf alg m, AuthErrorsM m) => Request -> SecretKey -> m Request
 checkRequestHmac req key = do
     tsig <- getBase64DecodedSignature req
@@ -92,10 +134,11 @@ checkRequestHmac req key = do
         | targetSig == actualSig = rerunRequestBody req chunks
         | otherwise = Mtl.throwError HashMismatch
 
--- | mostly for testing
+-- | signs a request in accordance with the config. mostly for testing.
 signRequest :: HashAlgorithm alg => RequestConfig alg -> Request -> SecretKey -> IO (Either AuthFailure Request)
 signRequest conf req k = runReaderT (runExceptT (addSignatureToRequest req k)) conf
 
+-- | the operation performed by 'signRequest'
 addSignatureToRequest :: (MonadIO m, HasReqConf alg m, AuthErrorsM m) => Request -> SecretKey -> m Request
 addSignatureToRequest req key = do
     (genSig, chunks) <- runWriterT $ hmacRequest req key
@@ -106,39 +149,56 @@ addSignatureToRequest req key = do
 
 -- ** manipulate the request 
 
+-- | constructs a new request with a 'requestBody' function that will
+-- produce each item in the input sequence until the sequence is empty.
 rerunRequestBody :: (Functor m, MonadIO m) => Request -> S.Seq BS.ByteString -> m Request
 rerunRequestBody req = fmap (setRequestBody req . produceChunked) . liftIO . newIORef
 
+-- | sets the request body to the IO action.
 setRequestBody :: Request -> IO BS.ByteString -> Request
 setRequestBody r b = r { requestBody = b }
 
+-- | gets the next chunk from the referenced sequence, returning mempty if
+-- it is already empty. (if it is not, then the IORef is updated to point
+-- to the next item in the sequence).
 produceChunked :: (Monoid a) => IORef (S.Seq a) -> IO a
 produceChunked ref = readIORef ref >>= (handleChunks . S.viewl)
     where
     handleChunks S.EmptyL = pure mempty
     handleChunks (h S.:< t) = writeIORef ref t *> pure h
 
+-- | add a header to a request (without checking for pre-existing headers)
 addHeader :: Request -> (HeaderName, BS.ByteString) -> Request
 addHeader r h = r { requestHeaders = h : requestHeaders r }
 
 -- ** computing the request signature
 
+-- | performs the full incremental hash/sign algorithm on the request and
+-- returns the signature.
 hmacRequest :: (MonadIO m, HasReqConf alg m, AuthErrorsM m, WriteChunks m) => Request -> SecretKey -> m (HMAC alg)
 hmacRequest req = hmacRequestInit >=> execStateT (addHashComponents req) >=> return . hmacFinalize
 
+-- | sets up the hash algorithm with the secret key
 hmacRequestInit :: HasReqConf alg m => SecretKey -> m (HMACContext alg)
 hmacRequestInit (SecretKey k) = flip hmacInitAlg k <$> Mtl.reader hashAlgorithm
 
 -- *** adding stuff to the hash
 
+-- | add all of the important components of the request to the hash 
+-- 
+-- - request method (newline)
+-- - timestamp header (newline)
+-- - api key (if necessary)
+-- - raw path info 
+-- - query params (with a question mark to separate from the path info)
+-- - (newline)
+-- - the body of the request
 addHashComponents :: (MonadIO m, HasReqConf alg m, AuthErrorsM m, WriteChunks m, HmacState alg m) => Request -> m ()
 addHashComponents = allRead [ 
     addToHash . requestMethod, addSep,
     addTimestampHeader, addSep, 
     ensureApiKeyIsAdded,
-    addToHash . rawPathInfo, addSep,
     addToHash . rawPathInfo,
-    --todo: proper formatting of query params
     addToHash . renderQuery True . queryString, addSep,
     addBodyToHash
     ]
@@ -146,46 +206,64 @@ addHashComponents = allRead [
     addSep = const (addToHash "\n")
     addTimestampHeader = getTimestampHeader >=> addToHash
 
+-- | ensure the hash/signature will include the api key value according to
+-- the spec - i.e. in either spec, this will fail the computation if the
+-- key is not present according to spec; this function will have no further
+-- effect for a query parameter key, but for a header key, it will add it
+-- to the hash.
 ensureApiKeyIsAdded :: (AuthErrorsM m, HasReqConf alg m, HmacState alg m) => Request -> m ()
 ensureApiKeyIsAdded req =  Mtl.reader keySpec >>= (maybe <$> Mtl.throwError . MissingApiKey <*> actionForSpec <*> flip getApiKey req)
     where
     actionForSpec (QueryParamKey _) _ = return ()
     actionForSpec (HeaderKey _) (ApiKey k) = addToHash k >> addToHash "\n"
 
+-- | keep getting chunks from the request and appending them to the hash
+-- (and also storing them in the writer value) until there are no more chunks
 addBodyToHash :: (MonadIO m, HmacState alg m, WriteChunks m) => Request -> m ()
 addBodyToHash req = whileJust_ (getNextChunkForHash req) $ \c -> addToHash c *> Mtl.tell (S.singleton c)
 
+-- | get the next chunk from the request body, if there is one
 getNextChunkForHash :: (MonadIO m, Functor m) => Request -> m (Maybe BS.ByteString)
 getNextChunkForHash = fmap (justUnless BS.null) . liftIO . requestBody
 
+-- | add a value to the incremental hash
 addToHash :: HmacState alg m => BS.ByteString -> m ()
 addToHash = Mtl.modify . flip hmacUpdate
 
 -- ** getting headers
 
+-- | get the signature header value, decode it from base64 url encoding,
+-- and then read it as a digest
 getBase64DecodedSignature :: (HasReqConf alg m, AuthErrorsM m) => Request -> m (HMAC alg)
 getBase64DecodedSignature = getSignatureHeader >=> 
                             either (Mtl.throwError . SignatureBase64DecodeFailed) return . B64.decode >=>
                             maybe (Mtl.throwError SignatureToDigestFailed) return . digestFromByteString >=>
                             return . HMAC
 
+-- | get the header that should contain the timestamp
 getTimestampHeader :: (AuthErrorsM m, HasReqConf alg m) => Request -> m BS.ByteString
 getTimestampHeader = getHeader timestampHeader MissingTimestampHeader
 
+-- | get the header that should contain the signature
 getSignatureHeader :: (AuthErrorsM m, HasReqConf alg m) => Request -> m BS.ByteString
 getSignatureHeader = getHeader signatureHeader MissingSignatureHeader
 
+-- | get a header from the request; throw an error if it can't be found
 getHeader :: (AuthErrorsM m, HasReqConf alg m) => (RequestConfig alg -> HeaderName) -> (HeaderName -> AuthFailure) -> Request -> m BS.ByteString
 getHeader targetHeader err req = Mtl.reader targetHeader >>= \header ->
     maybe (Mtl.throwError (err header)) return $ lookup header (requestHeaders req)
 
 -- * generic utilities
 
+-- | return the value if the predicate of it is true
 justWhen :: (a -> Bool) -> a -> Maybe a
 justWhen p a = if p a then Just a else Nothing
 
+-- | return the value if the predicate of it is false
 justUnless :: (a -> Bool) -> a -> Maybe a
 justUnless p a = if p a then Nothing else Just a
 
+-- | calls every function in the data structure, and then traverses/folds
+-- the contained actions.
 allRead :: (Applicative m, Foldable t) => t (a -> m b) -> a -> m ()
 allRead l v = for_ l  ($ v)
